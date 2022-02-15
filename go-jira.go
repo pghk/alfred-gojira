5894a42b96697f23da8d2479cf15e7e5c7a0311e
Paul Hendrick <paul.g.hendrick@gmail.com>
2022-02-14 22:17:29 -0500

    ✅ Record an API response into a test fixture

--- go-jira.go
+++ go-jira.go
@@ -5,8 +5,7 @@ import (
 	"github.com/go-jira/jira/jiradata"
 )
 
-func GetJiraList() (*jiradata.SearchResults, error) {
-	j := jira.NewJira("https://jira.atlassian.com")
+func GetJiraList(j jira.Jira) (*jiradata.SearchResults, error) {
 	o := &jira.SearchOptions{Project: "JRACLOUD"}
 	return j.Search(o)
 }
--- go-jira_test.go
+++ go-jira_test.go
@@ -2,11 +2,36 @@ package main
 
 import (
 	"fmt"
+	"github.com/go-jira/jira"
 	"testing"
+
+	"github.com/coryb/oreo"
+	"github.com/dnaeon/go-vcr/recorder"
 )
 
 func TestGetJira(t *testing.T) {
-	list, err := GetJiraList()
+	// Start test fixture recorder
+	vcr, err := recorder.New("testdata/fixtures/test")
+	if err != nil {
+		t.Fatal(err)
+	}
+	// Stop recorder once done
+	defer func(vcr *recorder.Recorder) {
+		err := vcr.Stop()
+		if err != nil {
+			t.Fatal(err)
+		}
+	}(vcr)
+
+	// Create an instance of the http client the go-jira library normally uses, injected with our recorder
+	recordingHttpClient := oreo.New()
+	recordingHttpClient.Client.Transport = vcr
+
+	// Inject our modified client into the new instance provided by the go-jira library
+	jiraClient := *jira.NewJira("https://jira.atlassian.com")
+	jiraClient.UA = recordingHttpClient
+
+	list, err := GetJiraList(jiraClient)
 	if err != nil {
 		t.Fatal(err)
 	}
